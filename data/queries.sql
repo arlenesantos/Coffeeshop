da328ec6941270cf8ce9cf11b527ec208fb8a6ff
Arlene Santos <arlenesantos89@gmail.com>
2022-03-26 15:37:47 +0000

    fix queries

--- data/queries.sql
+++ data/queries.sql
@@ -1,5 +1,5 @@
 -- usuarios registrados
-SELECT COUNT(id) 
+SELECT COUNT(id) AS customer_register 
 FROM customers;
 
 --usuarios com compras
@@ -10,7 +10,6 @@ WHERE checkout = true;
 --usuarios sem compras
 SELECT COUNT(customers.id) 
 FROM customers
-LEFT JOIN purchases ON customers.id = purchases.customer_id
 WHERE customers.id NOT IN (
     SELECT DISTINCT customer_id
     FROM purchases
@@ -29,7 +28,7 @@ FROM purchases
 WHERE checkout = true;
 
 -- faturamento todas as vendas
-SELECT SUM (pd.quantity * products.price)
+SELECT SUM (pd.quantity * products.price) AS revenue
 FROM purchaseDetail AS pd
 LEFT JOIN products ON pd.product_id = products.id
 LEFT JOIN purchases ON pd.purchase_id = purchases.id
@@ -50,7 +49,7 @@ WHERE checkout = true
 AND purchases.date BETWEEN CURRENT_DATE - 30 AND CURRENT_DATE;
 
 -- qtde media itens por compra
-SELECT AVG (quantity)::numeric(10,2)
+SELECT (SUM(quantity) / COUNT(DISTINCT purchase_id)::numeric(10,2))::numeric(10,2) AS average
 FROM purchaseDetail AS pd
 LEFT JOIN purchases ON pd.purchase_id = purchases.id
 WHERE purchases.checkout = true;
@@ -66,11 +65,15 @@ WHERE checkout = true
 GROUP BY purchases.date;
 
 -- ticket medio
-SELECT  AVG (pd.quantity * products.price)::numeric(10,2) 
-FROM purchaseDetail AS pd
-LEFT JOIN products ON pd.product_id = products.id
-LEFT JOIN purchases ON pd.purchase_id = purchases.id
-WHERE purchases.checkout = true;
+SELECT (total / purchase_count)::numeric(10,2) AS average_ticket
+FROM (
+    SELECT COUNT(DISTINCT pd.purchase_id) AS purchase_count, SUM(products.price * pd.quantity) AS total
+    FROM purchaseDetail AS pd
+    LEFT JOIN products ON pd.product_id = products.id
+    LEFT JOIN purchases ON pd.purchase_id = purchases.id
+    WHERE purchases.checkout = true
+) AS purchases_revenue;
+
 
 -- vendas por categoria
 -- PQ OVER???
@@ -85,6 +88,36 @@ GROUP BY categories.name;
 
 
 -- vendas/categoria por dia
+SELECT p.date, categories.name,
+(SUM(pd.quantity)::numeric(10,2) / dqt.day_quantity)::numeric(10,2) AS daily_percentage_category
+FROM purchases as p
+LEFT JOIN purchaseDetail as pd ON p.id = pd.purchase_id
+LEFT JOIN products ON products.id = pd.product_id
+LEFT JOIN categories ON products.category_id = categories.id
+LEFT JOIN (
+    SELECT purchases.date, SUM(pd.quantity)::numeric(10,2) AS day_quantity FROM purchases
+    LEFT JOIN purchaseDetail as pd ON purchases.id = pd.purchase_id
+    WHERE purchases.checkout = true
+    GROUP BY purchases.date
+) AS dqt on dqt.date = p.date
+WHERE checkout = true
+GROUP BY p.date,categories.name, dqt.day_quantity
+ORDER BY p.date,categories.name DESC;
+
+-- produto mais vendido --> qtde e faturamento
+SELECT DISTINCT products.id, products.name, brands.name AS brand, products.price, SUM(pd.quantity) AS sales, SUM (pd.quantity * products.price) AS total
+FROM purchaseDetail AS pd
+LEFT JOIN products ON pd.product_id = products.id
+LEFT JOIN purchases ON pd.purchase_id = purchases.id
+LEFT JOIN brands ON products.brand_id = brands.id
+WHERE purchases.checkout = true
+GROUP BY products.id, products.name, brand, products.price
+ORDER BY sales DESC, total DESC LIMIT 1;
+
+
+
+/*
+-- vendas/categoria por dia 
 SELECT DISTINCT n1.name, n1.date, n1.sales, n2.day_sum,
 (SUM (n1.sales) / SUM (n2.day_sum) * 100)::numeric(10,2) AS percentage
 FROM (
@@ -113,13 +146,8 @@ INNER JOIN (
 ON n2.date = n1.date
 GROUP BY n1.name, n1.date, n1.sales, n2.day_sum
 ORDER BY n1.date DESC;
+*/
 
--- produto mais vendido --> qtde e faturamento
-SELECT DISTINCT products.id, products.name, brands.name AS brand, products.price, SUM(pd.quantity) AS sales, SUM (pd.quantity * products.price) AS total
-FROM purchaseDetail AS pd
-LEFT JOIN products ON pd.product_id = products.id
-LEFT JOIN purchases ON pd.purchase_id = purchases.id
-LEFT JOIN brands ON products.brand_id = brands.id
-WHERE purchases.checkout = true
-GROUP BY products.id, products.name, brand, products.price
-ORDER BY sales DESC, total DESC;
+
+
+
